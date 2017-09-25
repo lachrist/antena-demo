5d5d11fe5929ba4134b4d61a6c45548f8d83b995
lachrist <lachrist@vub.ac.be>
2017-09-25 16:02:54 +0200

    npm ready

---
+++ README.md
@@ -0,0 +1,12 @@
+# Antena Demo
+
+Demonstration of the [antena](https://github/lachrist/antena) package.
+
+<!--
+mkdir node_modules
+npm install browserify
+cd node_modules/
+ln -s ../
+ln -s ../../../commonjs-playground/ commonjs-playground
+ln -s ../../../stdio-gui/ stdio-gui
+ -->
---
+++ bundle.js
@@ -0,0 +1,26 @@
+
+var Fs = require("fs");
+var Path = require("path");
+var Browserify = require("browserify");
+var CommonjsPlaygroundBundle = require("commonjs-playground/bundle");
+
+CommonjsPlaygroundBundle(Path.join(__dirname, "receptor.js"), function (error, rplayground) {
+  if (error)
+    throw error;
+  CommonjsPlaygroundBundle(Path.join(__dirname, "child.js"), function (error, cplayground) {
+    if (error)
+      throw error;
+    Fs.writeFileSync(Path.join(__dirname, "playgrounds.js"), "module.exports = "+JSON.stringify({
+      receptor: rplayground,
+      child: cplayground
+    }, null, 2)+";\n", {encoding:"utf8"});
+    Browserify(Path.join(__dirname, "script.js")).bundle(function (error, bundle) {
+      if (error)
+        throw error;
+      var html = Fs.readFileSync(Path.join(__dirname, "template.html"), "utf8").replace("<!-- SCRIPT -->", function () {
+        return "<script>"+bundle.toString("utf8").replace("</script>", "<\\/script>")+"</script>";
+      });
+      Fs.writeFileSync(Path.join(__dirname, "index.html"), html, {encoding:"utf8"});
+    });
+  })
+});
